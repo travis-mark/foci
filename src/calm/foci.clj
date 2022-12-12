4913ab50b352b65c47e2290f7a57a4256784f677
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2022-12-12 15:15:07 -0500

    bb init

---
+++ src/calm/foci.clj
@@ -0,0 +1,72 @@
+(ns calm.foci
+  (:require [com.biffweb :as biff]
+            [calm.foci.feat.app :as app]
+            [calm.foci.feat.auth :as auth]
+            [calm.foci.feat.home :as home]
+            [calm.foci.feat.worker :as worker]
+            [calm.foci.schema :refer [malli-opts]]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.test :as test]
+            [clojure.tools.logging :as log]
+            [ring.middleware.anti-forgery :as anti-forgery]
+            [nrepl.cmdline :as nrepl-cmd]))
+
+(def features
+  [app/features
+   auth/features
+   home/features
+   worker/features])
+
+(def routes [["" {:middleware [anti-forgery/wrap-anti-forgery
+                               biff/wrap-anti-forgery-websockets
+                               biff/wrap-render-rum]}
+              (keep :routes features)]
+             (keep :api-routes features)])
+
+(def handler (-> (biff/reitit-handler {:routes routes})
+                 (biff/wrap-inner-defaults {})))
+
+(def static-pages (apply biff/safe-merge (map :static features)))
+
+(defn generate-assets! [sys]
+  (biff/export-rum static-pages "target/resources/public")
+  (biff/delete-old-files {:dir "target/resources/public"
+                          :exts [".html"]}))
+
+(defn on-save [sys]
+  (biff/add-libs)
+  (biff/eval-files! sys)
+  (generate-assets! sys)
+  (test/run-all-tests #"calm.foci.test.*"))
+
+(def components
+  [biff/use-config
+   biff/use-random-default-secrets
+   biff/use-xt
+   biff/use-queues
+   biff/use-tx-listener
+   biff/use-outer-default-middleware
+   biff/use-jetty
+   biff/use-chime
+   (biff/use-when
+    :calm.foci/enable-beholder
+    biff/use-beholder)])
+
+(defn start []
+  (biff/start-system
+   {:calm.foci/chat-clients (atom #{})
+    :biff/features #'features
+    :biff/after-refresh `start
+    :biff/handler #'handler
+    :biff/malli-opts #'malli-opts
+    :biff.beholder/on-save #'on-save
+    :biff.xtdb/tx-fns biff/tx-fns
+    :biff/config "config.edn"
+    :biff/components components})
+  (generate-assets! @biff/system)
+  (log/info "Go to" (:biff/base-url @biff/system)))
+
+(defn -main [& args]
+  (start)
+  (apply nrepl-cmd/-main args))
